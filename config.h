340c3e94639090e0a7a6b5714009408838070c02
stuart nelson <stuartnelson3@gmail.com>
2018-10-17 14:10:02 +0200

    Use st and update audio toggle paths

--- config.h
+++ config.h
@@ -57,9 +57,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvt", NULL };
-#define AUDIOTOGGLECMD  "/home/stuartnelson/dotfiles/bin/wm/audio-toggle"
-#define AUDIOSETCMD  "/home/stuartnelson/dotfiles/bin/wm/audio-set"
+static const char *termcmd[]  = { "st", NULL };
+#define AUDIOTOGGLECMD  "/home/stuart/dotfiles/bin/wm/audio-toggle"
+#define AUDIOSETCMD  "/home/stuart/dotfiles/bin/wm/audio-set"
 static const char *mutecmd[] = { AUDIOTOGGLECMD, NULL };
 static const char *decvolcmd[] = { AUDIOSETCMD, "5%-", NULL };
 static const char *incvolcmd[] = { AUDIOSETCMD, "5%+", NULL };
