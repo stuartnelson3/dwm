ea740e10d436cc04224e5a68fc710959ef27134c
stuart nelson <stuartnelson3@gmail.com>
2018-11-29 19:11:55 +0100

    tron theme
    still need to fix top level color

--- config.h
+++ config.h
@@ -5,17 +5,21 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Consolas:size=11" };
-static const char dmenufont[]       = "Consolas:size=11";
+static const char *fonts[]          = { "Consolas:style=Bold:pixelsize=14:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Consolas:style=Bold:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_tronfg[]      = "#aacfd1";
+static const char col_tronbg[]      = "#05080d";
+static const char col_trongray[]    = "#262828";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_tronbg, col_trongray },
+	[SchemeSel]  = { col_gray4, col_tronbg,  col_tronfg  },
 };
 
 /* tagging */
@@ -56,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_tronbg, "-nf", col_trongray, "-sb", col_tronbg, "-sf", col_tronfg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 #define AUDIOTOGGLECMD  "/home/stuart/dotfiles/bin/wm/audio-toggle"
 #define AUDIOSETCMD  "/home/stuart/dotfiles/bin/wm/audio-set"
