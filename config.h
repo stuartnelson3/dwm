745a7b47673a88db645848ff91f5ed0849dc90e4
stuart nelson <stuartnelson3@gmail.com>
2018-12-30 15:26:41 +0100

    Use alacritty

--- config.h
+++ config.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_tronbg, "-nf", col_trongray, "-sb", col_tronbg, "-sf", col_tronfg, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 #define AUDIOTOGGLECMD  "/home/stuart/dotfiles/bin/wm/audio-toggle"
 #define AUDIOSETCMD  "/home/stuart/dotfiles/bin/wm/audio-set"
 static const char *mutecmd[] = { AUDIOTOGGLECMD, NULL };
